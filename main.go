063e2aefdac4377726f23437f26db4efbd622bb0
Shinya Kato <k.shin220@gmail.com>
2019-08-25 07:36:46 +0000

    Add integer constant

--- main.go
+++ main.go
@@ -1,10 +1,65 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+  "unicode"
+)
+
+type Token struct {
+  int_value int
+}
+
+func tokenize(src string) []*Token {
+  chars := []rune(src)
+  pos := 0
+
+  tokens := []*Token {}
+  for {
+    if pos == len(src) {
+      break
+    }
+
+    switch {
+    case unicode.IsSpace(chars[pos]):
+      pos++
+
+    case unicode.IsDigit(chars[pos]):
+      int_value := 0
+      for pos < len(src) && unicode.IsDigit(chars[pos]) {
+        int_value = int_value * 10 + int(chars[pos] - '0')
+        pos++
+      }
+
+      tokens = append(tokens, &Token {
+        int_value: int_value,
+      })
+
+    default:
+      panic(fmt.Sprintf("tokenize: unexpected character: %c.", chars[pos]))
+    }
+  }
+
+  return tokens
+}
 
 func main() {
-  fmt.Println("  .global main")
-  fmt.Println("main:")
-  fmt.Println("  movl $42, %eax")
-  fmt.Println("  ret")
+  cmd := os.Args[0]
+  args := os.Args[1:]
+  if len(args) != 1 {
+    fmt.Fprintf(os.Stderr, "usage: %s [input srouce code]\n", cmd)
+    os.Exit(1)
+  }
+
+  src := args[0]
+  tokens := tokenize(src)
+
+  if len(tokens) != 1 {
+    panic("only one integer constant is expected.")
+  }
+
+  fmt.Printf("  .global main\n")
+  fmt.Printf("main:\n")
+  fmt.Printf("  movl $%d, %%eax\n", tokens[0].int_value)
+  fmt.Printf("  ret\n")
 }
--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,32 @@
 mkdir -p tests/tmp
 
-./skgo > tests/tmp/out.s
-gcc -o tests/tmp/out tests/tmp/out.s
-./tests/tmp/out
+example() {
+  expr=$1
+  expected=$2
 
-if [ $? -ne 42 ]; then
-  echo 'NG'
-  exit 1
-fi
+  ./skgo "$expr" > tests/tmp/out.s
+  if [ $? -ne 0 ]; then
+    echo "NG: failed to compile: $expr."
+    exit 1
+  fi
+
+  gcc -o tests/tmp/out tests/tmp/out.s
+  if [ $? -ne 0 ]; then
+    echo "NG: failed to link with GCC: $expr."
+    exit 1
+  fi
+
+  ./tests/tmp/out
+  actual=$?
+  if [ $actual -ne $expected ]; then
+    echo "NG: $expected is expected, but got $actual: $expr."
+    exit 1
+  fi
+}
+
+example '42' 42
+example '35' 35
+example '0' 0
 
 echo 'OK'
 exit 0
